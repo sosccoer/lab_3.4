4387ed850c1a6c8c56aa059e1776d5a894070254
VrodeYuliya <romanova.yulia5444@gmail.com>
2024-05-22 21:10:31 +0300

    Update JoinedToast.kt

--- app/src/main/java/com/topic3/android/reddit/components/JoinedToast.kt
+++ app/src/main/java/com/topic3/android/reddit/components/JoinedToast.kt
@@ -26,7 +26,13 @@ import com.topic3.android.reddit.R
 
 @Composable
 fun JoinedToast(visible: Boolean){
-    ToastContent()
+    AnimatedVisibility(
+        visible = visible,
+        enter = slideInVertically(initialOffsetY = { +40 }) + fadeIn(),
+        exit = slideOutVertically() + fadeOut()
+    ) {
+        ToastContent()
+    }
 }
 
 @Composable
